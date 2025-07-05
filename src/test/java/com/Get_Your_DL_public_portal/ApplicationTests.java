2d2776c9b4e9b8e6a91682a985538f1b92fe9b88
Bhavesh Kaushik <128074859+bk2712@users.noreply.github.com>
2025-07-05 22:34:35 +0530

    First commit

---
+++ src/test/java/com/Get_Your_DL_public_portal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Get_Your_DL_public_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
